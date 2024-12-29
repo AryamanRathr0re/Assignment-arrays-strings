23e6eba9416f5e9025b92700bfc37e5fc9b42cd9
AryamanRathr0re <aryanrathore301@gmail.com>
2024-12-29 20:25:35 +0530

    this is second question

---
+++ L0_Managing_a_To_Do_List.js
@@ -0,0 +1,9 @@
+
+let dailyToDo = ["Coding", "Gaming", "Workout", "Study", "Assignment"]
+console.log("Array Before the operation: ", dailyToDo)
+
+
+dailyToDo.pop()
+
+dailyToDo.unshift("Songs")
+console.log("Array Before the operation: ", dailyToDo)
